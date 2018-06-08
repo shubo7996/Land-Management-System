8dc71c7dfe982c3ac39e354d21ada86da2c3cc2f
shubo7996 <whatsupmofo420@gmail.com>
2018-06-08 23:02:10 +0530

    final commit

--- TataSteel/Controllers/HomeController.cs
+++ TataSteel/Controllers/HomeController.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
+using TataSteel.Models;
+using System.Web.Helpers;
+
 
 namespace TataSteel.Controllers
 {
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
             return View();
@@ -22,8 +28,45 @@ namespace TataSteel.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            return View();
+        }
 
+        [HttpPost]
+        public ActionResult Contact(string receiver, string message, string subject)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var senderEmail = new MailAddress("whatsupmofo420@gmail.com", "Subho");
+                    var receiverEmail = new MailAddress(receiver, "Receiver");
+                    var password = "subho@1997";
+                    var sub = subject;
+                    var body = message;
+                    var SmtpClient = new SmtpClient
+                    {
+                        Host = "smtp.gmail.com",
+                        Port = 587,
+                        EnableSsl = true,
+                        DeliveryMethod = SmtpDeliveryMethod.Network,
+                        UseDefaultCredentials = false,
+                        Credentials = new NetworkCredential(senderEmail.Address, password)
+                    };
+                    using (var mess = new MailMessage(senderEmail, receiverEmail)
+                    {
+                        Subject = subject,
+                        Body = body
+                    })
+                    {
+                        SmtpClient.Send(mess);
+                    }
+                    return View();
+                }
+            }
+            catch (Exception)
+            {
+                ViewBag.Error = "Some Error";
+            }
             return View();
         }
     }
---
+++ TataSteel/Models/Email.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TataSteel.Models
+{
+    public class Email
+    {
+        public string Receiver
+        {
+            get;
+            set;
+        }
+        
+        public string Subject
+        {
+            get;
+            set;
+        }
+        public string Message
+        {
+            get;
+            set;
+        }
+
+    }
+}
